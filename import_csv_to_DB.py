72571aa17decc6313d3c80efcd529162b0f46178
wojter <wojtektro2@gmail.com>
2023-03-17 21:17:29 +0100

    add: import credits to DB

--- import_csv_to_DB.py
+++ import_csv_to_DB.py
@@ -13,23 +13,14 @@ if __name__ == "__main__":
 
     title_data = pd.read_csv('titles.csv')
     t_df = pd.DataFrame(title_data)
-    # t_df.to_sql('titles', engine, if_exists="replace", index=False)
-    # movies_clean = t_df.dropna(
-    #     subset=['imdb_id', 'imdb_score', 'imdb_votes', 'tmdb_popularity', 'tmdb_score'])
-    # mv_genres = movies_clean.genres
 
-    # for row in movies_clean.itertuples():
-    #     print(row.genres)
-    # print(df.duplicated().sum())
-    # print(df.isnull().sum())
-    # print(df.head())
-    # print(movies_clean.genres.value_counts())
-    # res = t_df.to_sql(name="titles", con=con, if_exists="replace", index=True)
-    droped = t_df.drop(columns=["id",'age_certification','seasons','genres','production_countries','imdb_id', 'imdb_score', 'imdb_votes', 'tmdb_popularity', 'tmdb_score'])
+    t_df.rename(columns = {'id':'person_id'})
+    droped = t_df.drop(columns=['age_certification','seasons','genres','production_countries','imdb_id', 'imdb_score', 'imdb_votes', 'tmdb_popularity', 'tmdb_score'])
     res = droped.to_csv( "main_coma.csv", index=False, index_label='id', sep=',')
 
     sql = '''CREATE TABLE titles(
         id SERIAL PRIMARY KEY,
+        title_id VARCHAR(10),
         title varchar(200),
         type varchar(10),
         description text,
@@ -44,12 +35,26 @@ if __name__ == "__main__":
         next(reader)
         for row in reader:
             cur.execute(
-                "INSERT INTO titles VALUES (DEFAULT, %s, %s, %s, %s, %s)", row
+                "INSERT INTO titles VALUES (DEFAULT, %s, %s, %s, %s, %s, %s)", row
+            )
+    conn.commit()
+
+    sql = '''CREATE TABLE credits(
+        id int,
+        title_id VARCHAR(10),
+        name varchar(200),
+        character varchar(1000),
+        role varchar(30)
+    )'''
+    cur.execute(sql) 
+    conn.commit()
+
+    with open('credits.csv', newline='',encoding="utf-8") as f:
+        reader = csv.reader(f, delimiter=',')
+        next(reader)
+        for row in reader:
+            cur.execute(
+                "INSERT INTO credits VALUES (%s, %s, %s, %s, %s)", row
             )
     conn.commit()
     conn.close()
-
-    # titles_count = cur.execute("SELECT genres, count(genres) FROM titles group by genres ").fetchall()
-    # print(titles_count)
-    # cur.execute("SELECT COUNT(*) as credits_count FROM credits")
-    # print(cur.fetchall())
